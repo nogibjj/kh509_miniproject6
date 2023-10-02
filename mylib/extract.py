0d425041d980ea3ddbcdd49b0685554c0b5dbafe
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-10-02 03:16:04 +0000

    fix fileline

--- mylib/extract.py
+++ mylib/extract.py
@@ -8,7 +8,7 @@ import requests
 
 def extract(url=
 "https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
-file_path="data/GroceryDB_IgFPro.csv"):
+file_path="GroceryDB_IgFPro.csv"):
     """"Extract a url to a file path"""
     with requests.get(url) as r:
         with open(file_path, 'wb') as f:
