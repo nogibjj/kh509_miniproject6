148068f90e99560dddcad572e08d6ecaa80556ce
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-10-01 21:58:40 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,51 +1,90 @@
-"""
-Test goes here
-
-"""
 import unittest
 import sqlite3
-from mylib.query import see_five_query, insert_query, update_query, order_query
-from mylib.transform_load import load
+from your_module_name import head_query, insert_query  # Replace 'your_module_name' with the actual module name where your functions are defined
+from prettytable import PrettyTable
 
+class TestDatabaseFunctions(unittest.TestCase):
+    def setUp(self):
+        # Set up a temporary database for testing
+        self.conn = sqlite3.connect(":memory:")
+        self.cursor = self.conn.cursor()
+        # Create a test table
+        self.cursor.execute('''
+            CREATE TABLE GroceryDB (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                general_name TEXT,
+                count_products INTEGER,
+                ingred_FPro REAL,
+                avg_FPro_products REAL,
+                avg_distance_root REAL,
+                ingred_normalization_term REAL,
+                semantic_tree_name TEXT,
+                semantic_tree_node TEXT
+            )
+        ''')
+        self.conn.commit()
 
-class TestElectricityFunctionsPartOne(unittest.TestCase):
-    def test_load_function(self):
-        """TESTING THE CREATION OF A DATABASE, i.e., testing transform_load.py"""
-        db_file = load("test_dataset.csv")
-        self.assertTrue(db_file == "Electricity.db")
-        conn = sqlite3.connect(db_file)
-        cursor = conn.cursor()
-        cursor.execute(
-            "SELECT name FROM sqlite_master WHERE type='table' AND name='Electricity';"
-        )
-        table_exists = cursor.fetchone()
-        self.assertIsNotNone(table_exists)
-        cursor.execute("SELECT COUNT(*) FROM Electricity;")
-        row_count = cursor.fetchone()[0]
-        self.assertEqual(row_count, 37792)
-        conn.close()
+    def tearDown(self):
+        # Close the database connection
+        self.conn.close()
 
     def test_head_query(self):
-        """Testing is the size of the see_fice_query output is 5"""
-        result = see_five_query()
-        num_rows = len(result)
-        self.assertEqual(num_rows, 5)
+        # Insert test data into the table
+        self.cursor.executemany('''
+            INSERT INTO GroceryDB 
+            (general_name, count_products, ingred_FPro, avg_FPro_products, 
+            avg_distance_root, ingred_normalization_term, 
+            semantic_tree_name, semantic_tree_node)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', [
+            ('item1', 1, 0.1, 0.2, 0.3, 0.4, 'tree1', 'node1'),
+            ('item2', 2, 0.2, 0.3, 0.4, 0.5, 'tree2', 'node2'),
+            ('item3', 3, 0.3, 0.4, 0.5, 0.6, 'tree3', 'node3'),
+        ])
+        self.conn.commit()
+
+        # Redirect stdout to capture print output
+        import sys
+        captured_output = sys.stdout
+        sys.stdout = self.buffer = io.StringIO()
+
+        # Call the head_query function
+        head_query()
+
+        # Reset stdout
+        sys.stdout = captured_output
+
+        # Get the printed output
+        printed_output = self.buffer.getvalue()
+
+        # Assert the printed output contains expected content
+        self.assertIn("Top 5 rows of the GroceryDB table:", printed_output)
+        self.assertIn("item1", printed_output)
+        self.assertIn("item2", printed_output)
+        self.assertIn("item3", printed_output)
 
     def test_insert_query(self):
-        """Testing INSERT query"""
-        conn = sqlite3.connect("GroceryDB.db")
-        cursor = conn.cursor()
+        # Call the insert_query function
         insert_query()
-        cursor.execute(
-            """
-            SELECT zip FROM Electricity WHERE 
-            utility_name = 'Data Engineering Electric';
-            """
-        )
-        result = cursor.fetchone()
-        self.assertEqual(result[0], 55555)
-        conn.close()
 
+        # Query the database to check if the row was inserted
+        self.cursor.execute("SELECT * FROM GroceryDB")
+        rows = self.cursor.fetchall()
 
-if __name__ == "__main__":
+        # Assert that there is one row in the table
+        self.assertEqual(len(rows), 1)
+
+        # Assert the values of the inserted row
+        inserted_row = rows[0]
+        self.assertEqual(inserted_row[1], 'folgers coffee')
+        self.assertEqual(inserted_row[2], 11)
+        self.assertEqual(inserted_row[3], 0.21)
+        self.assertEqual(inserted_row[4], 0.2254401549508692)
+        self.assertEqual(inserted_row[5], 4.3)
+        self.assertEqual(inserted_row[6], 15.2222)
+        self.assertIsNone(inserted_row[7])
+        self.assertIsNone(inserted_row[8])
+
+if __name__ == '__main__':
     unittest.main()
+
