b410b4c1e56b6ff22d5e031a21ec1fa694a4b817
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-10-01 21:24:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,27 @@ ETL-Query script
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
+import fire
 
-# Extract
-print("Extracting data...")
-extract()
 
-# Transform and load
-print("Transforming data...")
-load()
+def main(query_statement):
+    # Extract
+    print("Extracting data...")
+    extract()
 
-# Query
-print("Querying data...")
-query()
+    # Transform and load
+    print("Transforming data...")
+    load()
+
+    print("Querying data...")
+    query(query_statement)
+
+
+# Following below are sample CRUD Operations
+# """SELECT general_name FROM GroceryDB WHERE count_products > 10"""
+# """ DELETE FROM GroceryDB WHERE general_name LIKE 'locust bean gum' """
+# """SELECT general_name FROM GroceryDB WHERE general_name LIKE '%bean%'"""
+
+# main(sql_statement)
+if __name__ == "__main__":
+    fire.Fire(main)
